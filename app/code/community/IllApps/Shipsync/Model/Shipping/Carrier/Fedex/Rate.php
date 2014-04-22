5295eb05b5e3a3928adbde6ba81532f9bb31ff2c
David Scott Kirby <davidkirby@homophoni.com>
2014-04-22 17:58:07 -0400

    Added free shipping exclusion flag for AK & HI

--- app/code/community/IllApps/Shipsync/Model/Shipping/Carrier/Fedex/Rate.php
+++ app/code/community/IllApps/Shipsync/Model/Shipping/Carrier/Fedex/Rate.php
@@ -681,10 +681,13 @@ class IllApps_Shipsync_Model_Shipping_Carrier_Fedex_Rate extends IllApps_Shipsyn
                 //Test for Continental US destination
                 $freeMethodLimit = Mage::getStoreConfig('carriers/fedex/free_shipping_subtotal');
                 $destCode        = $this->_rateRequest->getDestRegionCode();
-                $destContinental = ($destCode == 'AK' || $destCode == 'HI') ? false : true;
-                $continentalTest = (($freeMethodLimit && $destContinental) || !$freeMethodLimit) ? true : false;
-                
-                if (Mage::getStoreConfig('carriers/fedex/free_shipping_enable') && $continentalTest) {
+
+                if (Mage::getStoreConfig('carriers/fedex/free_shipping_exclusion') &&
+                    (($destCode == 'AK') || ($destCode == 'HI'))) {
+                        $exclude = true;
+                }
+
+                if (Mage::getStoreConfig('carriers/fedex/free_shipping_enable') && !$exclude) {
                     if (Mage::getStoreConfig('carriers/fedex/free_shipping_discounts')) {
                         $value = $this->_rateRequest->getValue();
                     } else {
